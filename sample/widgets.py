d1d213f3bd5fede3b0ca4bc070f25fce6830b90c
mattjmorrison <mattjmorrison@mattjmorrison.com>
2013-12-15 00:53:54 -0600

    Feature: removed unneeded code

--- sample/widgets.py
+++ sample/widgets.py
@@ -14,15 +14,6 @@ class ImageCheckboxChoiceInput(widgets.CheckboxChoiceInput):
 class ImageCheckBoxSelectMultipleRenderer(widgets.CheckboxFieldRenderer):
     choice_input_class = ImageCheckboxChoiceInput
 
-    def render(self):
-        id_ = self.attrs.get('id', None)
-        start_tag = format_html('<ul id="{0}">', id_) if id_ else '<ul>'
-        output = [start_tag]
-        for widget in self:
-            output.append(format_html('<li>{0}</li>', mark_safe(widget)))
-        output.append('</ul>')
-        return mark_safe('\n'.join(output))
-
 
 class ImageCheckBoxSelectMultiple(forms.CheckboxSelectMultiple):
     renderer = ImageCheckBoxSelectMultipleRenderer
